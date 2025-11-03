8e3d985e00c95f3c3005350f36dfa4f5d72a80b3
Miguel Villa Floran <miguel.villafloran@gmail.com>
2025-11-03 02:05:53 -0800

    Nix packages

--- shell.nix
+++ shell.nix
@@ -10,8 +10,29 @@ pkgs.mkShell {
     cmake-format # CMake format tool
     nixfmt # Nix formatter
     just # Just runner
+    xorg.libX11
+    xorg.libXrandr
+    xorg.libXinerama
+    xorg.libXcursor
+    xorg.libXi
+    xorg.libXext
+    mesa
+    libGL
   ];
 
   # Shell hook to set up environment
-  shellHook = "";
+  shellHook = ''
+    export LD_LIBRARY_PATH=${
+      pkgs.lib.makeLibraryPath [
+        pkgs.xorg.libX11
+        pkgs.xorg.libXrandr
+        pkgs.xorg.libXinerama
+        pkgs.xorg.libXcursor
+        pkgs.xorg.libXi
+        pkgs.xorg.libXext
+        pkgs.libGL
+        pkgs.mesa
+      ]
+    }:$LD_LIBRARY_PATH
+  '';
 }
